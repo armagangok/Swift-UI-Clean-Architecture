d257c926633da96411969c472c9041e756ea6336
Armağan Gök <1armagangok@gmail.com>
2023-01-29 22:10:21 +0300

    Initial Commit

---
+++ CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Armağan Gök on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
